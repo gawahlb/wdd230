14f0e8fe754977e93d674d1c29a36efea6f49481
gawahlb <wah23001@byui.edu>
2024-05-03 15:58:17 -0700

    added hamburger menu to home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,3 +9,12 @@ const options = { month: 'long', day: 'numeric', year: 'numeric' };
 let latestSave = currentSave.toLocaleDateString('en-us', options);
 let footerSaveDate = document.getElementById('lastModified');
 footerSaveDate.textContent = latestSave;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
