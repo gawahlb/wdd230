b7c4fa1abe716a92eb484723d2ed18f48c047c58
gawahlb <wah23001@byui.edu>
2024-05-04 21:06:43 -0700

    updated dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -23,9 +23,17 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
 const header = document.querySelector("header");
+const section = document.querySelector(".learning-card");
+const h2 = document.querySelector("h2");
+const infoH2 = document.querySelector(".info-h2");
+const infoCard = document.querySelector(".info-card");
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle('dark-mode')
     main.classList.toggle('dark-mode');
     header.classList.toggle('header-dark-mode');
+    section.classList.toggle('section-dark-mode');
+    infoH2.classList.toggle('section-dark-mode');
+    h2.classList.toggle('section-dark-mode');
+    infoCard.classList.toggle('section-dark-mode');
 });
