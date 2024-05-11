4eeab20bc9e5f85a1f6c6beb6b40b9f4cfd12c44
gawahlb <wah23001@byui.edu>
2024-05-11 12:57:57 -0700

    updated discover page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -11,3 +11,23 @@ const options = { month: 'long', day: 'numeric', year: 'numeric' };
 let latestSave = currentSave.toLocaleDateString('en-us', options);
 let footerSaveDate = document.getElementById('lastModified');
 footerSaveDate.textContent = latestSave;
+
+
+const visitDisp = document.querySelector(".visits");
+const msToDays = 86400000;
+const dateToday = new Date();
+const today = Date.now();
+
+let lastVisit = Number(window.localStorage.getItem("lastVisited"));
+
+if (lastVisit == null) {
+    visitDisp.textContent = `Welcome! Let us know if you have any questions.`;
+} else if ((today - lastVisit) < msToDays) {
+    visitDisp.textContent = `Back so soon? Awesome!`;
+} else {
+    let daysAgo = (today - lastVisit) / msToDays;
+    visitDisp.textContent = `You last visited ${daysAgo.toFixed(0)} days ago.`
+
+}
+
+localStorage.setItem("lastVisited", today);
