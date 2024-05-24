14837e84d66871bc934a86b2bd6cc6342eece891
gawahlb <wah23001@byui.edu>
2024-05-24 13:54:28 -0700

    updating home page with JSON and Weather API

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -51,3 +51,35 @@ if (numVisits !== 0) {
 numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
+
+const currentWeather = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const weatherDesc = document.querySelector('#weather-desc');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.19&lon=-111.55&units=imperial&appid=b621ddf1b42de9cf4add4ef7671da79c';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentWeather.innerHTML = `${data.main.temp}&deg;F`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `Weather Icon`);
+    weatherDesc.textContent = `${desc}`;
+}
+
+apiFetch();
